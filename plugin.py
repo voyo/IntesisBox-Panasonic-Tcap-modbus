576106be96c5798fd05102d4a46894ca9fc98b07
Wojtek Sawasciuk <voyo@no-ip.pl>
2023-12-02 18:15:40 +0100

    add description about pyModbusTCP and mbusd

--- plugin.py
+++ plugin.py
@@ -7,6 +7,10 @@ Requirements:
     1.python module minimalmodbus -> http://minimalmodbus.readthedocs.io/en/master/
         (pi@raspberrypi:~$ sudo pip3 install minimalmodbus)
     2.Communication module Modbus USB to RS485 converter module
+    3.python module pyModbusTCP
+    4.'something' what can act as proxy for Modbus TCP to Modbus RTU (RS-232/485) connectivity. Hardware or software solution.
+      I'm using open-source mbusd (https://github.com/3cky/mbusd/ , running as docker service on my RBPI
+      
 """
 """
 <plugin key="Panasonic-IntesisBox" name="Panasonic-IntesisBox" version="0.9" author="voyo@no-ip.pl">
